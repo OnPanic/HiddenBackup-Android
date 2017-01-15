2aab82852111ce6435eb40d16f43ff13d04136bc
arrase <arrase@gmail.com>
2017-01-15 23:11:18 +0100

    base

---
+++ app/src/main/java/org/onpanic/hiddenbackup/fragments/BackupNow.java
@@ -0,0 +1,27 @@
+package org.onpanic.hiddenbackup.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.onpanic.hiddenbackup.R;
+
+public class BackupNow extends Fragment {
+
+
+    public BackupNow() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_backup_now, container, false);
+    }
+
+}
---
+++ app/src/main/java/org/onpanic/hiddenbackup/fragments/DirsFragment.java
@@ -0,0 +1,27 @@
+package org.onpanic.hiddenbackup.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.onpanic.hiddenbackup.R;
+
+public class DirsFragment extends Fragment {
+
+
+    public DirsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.dirs_fragment_item, container, false);
+    }
+
+}
