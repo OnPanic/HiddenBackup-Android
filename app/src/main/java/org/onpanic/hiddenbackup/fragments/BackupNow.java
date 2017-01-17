5c9e021dcda08292bb413f91df3ff17d8cfddd09
arrase <arrase@gmail.com>
2017-01-17 21:11:26 +0100

    animation

--- app/src/main/java/org/onpanic/hiddenbackup/fragments/BackupNow.java
+++ app/src/main/java/org/onpanic/hiddenbackup/fragments/BackupNow.java
@@ -2,14 +2,24 @@ package org.onpanic.hiddenbackup.fragments;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.onpanic.hiddenbackup.R;
 
+import info.guardianproject.netcipher.proxy.OrbotHelper;
+
 public class BackupNow extends Fragment {
+    private ImageView image;
+    private Context mContext;
+    private Animation animation;
 
     public BackupNow() {
         // Required empty public constructor
@@ -19,7 +29,25 @@ public class BackupNow extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_big_action_button, container, false);
+        View v = inflater.inflate(R.layout.fragment_big_action_button, container, false);
+        image = (ImageView) v.findViewById(R.id.big_action_button);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animation = AnimationUtils.loadAnimation(mContext, R.anim.clockwise);
+                image.startAnimation(animation);
+                OrbotHelper.requestStartTor(mContext);
+            }
+        });
+
+        TextView text = (TextView) v.findViewById(R.id.big_action_button_summary);
+        text.setText(mContext.getString(R.string.run_backup));
+        return v;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
 }
