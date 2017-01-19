f0c286dc76dce0efb105ab77834aaed2176c6a59
arrase <arrase@gmail.com>
2017-01-19 16:32:36 +0100

    repeat alarm

--- app/src/main/java/org/onpanic/hiddenbackup/services/SchedulerService.java
+++ app/src/main/java/org/onpanic/hiddenbackup/services/SchedulerService.java
@@ -1,18 +1,28 @@
 package org.onpanic.hiddenbackup.services;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 
+import org.onpanic.hiddenbackup.R;
 import org.onpanic.hiddenbackup.constants.HiddenBackupConstants;
+
+import java.util.Calendar;
+import java.util.UUID;
 
 public class SchedulerService extends Service {
     private LocalBroadcastManager localBroadcastManager;
     private BroadcastReceiver stopReceiver;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
 
     public SchedulerService() {
     }
@@ -21,6 +31,7 @@ public class SchedulerService extends Service {
     public void onCreate() {
         super.onCreate();
         localBroadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
+        alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
     }
 
     @Override
@@ -30,17 +41,50 @@ public class SchedulerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, final int startId) {
+        String action = intent.getAction();
 
-        stopReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                localBroadcastManager.unregisterReceiver(stopReceiver);
-                stopSelf(startId);
-            }
-        };
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        localBroadcastManager.registerReceiver(
-                stopReceiver, new IntentFilter(HiddenBackupConstants.ACTION_STOP_SCHEDULER));
+        if (action.equals(HiddenBackupConstants.ACTION_START_SCHEDULER)) {
+            // Rand action for extra securety
+            String rand = UUID.randomUUID().toString();
+            SharedPreferences.Editor edit = preferences.edit();
+            edit.putString(getString(R.string.pref_scheduler_rand), rand);
+            edit.apply();
+
+            Intent service = new Intent(this, SchedulerService.class);
+            service.setAction(rand);
+            alarmIntent = PendingIntent.getBroadcast(this, 0, service, 0);
+
+            String[] time = preferences.getString(getString(R.string.pref_scheduler_time), "00:00").split(":");
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]));
+            calendar.set(Calendar.MINUTE, Integer.parseInt(time[1]));
+
+            alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, alarmIntent);
+
+            stopReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    localBroadcastManager.unregisterReceiver(stopReceiver);
+                    alarmMgr.cancel(alarmIntent);
+                    stopSelf(startId);
+                }
+            };
+
+            localBroadcastManager.registerReceiver(
+                    stopReceiver, new IntentFilter(HiddenBackupConstants.ACTION_STOP_SCHEDULER));
+
+        } else if (action.equals(preferences.getString(getString(R.string.pref_scheduler_rand), UUID.randomUUID().toString()))) {
+            SharedPreferences.Editor edit = preferences.edit();
+            edit.putString(getString(R.string.pref_scheduler_rand), null);
+            edit.apply();
+
+            startService(new Intent(this, BackupService.class));
+        }
 
         return Service.START_STICKY;
     }
