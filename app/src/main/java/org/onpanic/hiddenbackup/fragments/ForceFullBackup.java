257615e0f5832d9665e15520b0c3e126a1c6d7ab
arrase <arrase@gmail.com>
2017-01-22 19:05:27 +0100

    feedback

--- app/src/main/java/org/onpanic/hiddenbackup/fragments/ForceFullBackup.java
+++ app/src/main/java/org/onpanic/hiddenbackup/fragments/ForceFullBackup.java
@@ -2,15 +2,19 @@ package org.onpanic.hiddenbackup.fragments;
 
 
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.onpanic.hiddenbackup.R;
 import org.onpanic.hiddenbackup.constants.HiddenBackupConstants;
@@ -18,6 +22,9 @@ import org.onpanic.hiddenbackup.services.OrbotService;
 
 public class ForceFullBackup extends Fragment {
     private Context mContext;
+    private ImageView image;
+    private LocalBroadcastManager localBroadcastManager;
+    private BroadcastReceiver backupFinish;
 
     public ForceFullBackup() {
         // Required empty public constructor
@@ -30,12 +37,26 @@ public class ForceFullBackup extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_big_action_button, container, false);
 
-        ImageView image = (ImageView) v.findViewById(R.id.big_action_button);
+        image = (ImageView) v.findViewById(R.id.big_action_button);
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.clockwise);
                 view.startAnimation(animation);
+                localBroadcastManager = LocalBroadcastManager.getInstance(mContext);
+
+                backupFinish = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        localBroadcastManager.unregisterReceiver(backupFinish);
+                        image.clearAnimation();
+                        Toast.makeText(mContext, R.string.runned_notification_content, Toast.LENGTH_LONG).show();
+                    }
+                };
+
+                localBroadcastManager.registerReceiver(
+                        backupFinish, new IntentFilter(HiddenBackupConstants.BACKUP_FINISH));
+
                 Intent intent = new Intent(mContext, OrbotService.class);
                 intent.setAction(HiddenBackupConstants.FULL_BACKUP);
                 mContext.startService(intent);
