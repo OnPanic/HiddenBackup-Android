35b25d494754bd245aaf1795f9eec4f823f84264
arrase <arrase@gmail.com>
2017-01-25 23:43:08 +0100

    merge and pendingintent

--- app/src/main/java/org/onpanic/hiddenbackup/receivers/ConnectionChangeReceiver.java
+++ app/src/main/java/org/onpanic/hiddenbackup/receivers/ConnectionChangeReceiver.java
@@ -1,5 +1,6 @@
 package org.onpanic.hiddenbackup.receivers;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,7 +8,6 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 import org.onpanic.hiddenbackup.R;
 
@@ -25,13 +25,19 @@ public class ConnectionChangeReceiver extends BroadcastReceiver {
                 && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI;
 
         if (wifiOn) {
-            Log.d("HIDDEN_BACKUP", "WIFI_ON");
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
             if (preferences.getBoolean(context.getString(R.string.pref_scheduler_delayed), false)) {
                 SharedPreferences.Editor edit = preferences.edit();
                 edit.putBoolean(context.getString(R.string.pref_scheduler_delayed), false);
                 edit.apply();
-                context.sendBroadcast(new Intent(context, ScheduledBackupReceiver.class));
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, RunBackupReceiver.class), 0);
+
+                try {
+                    pendingIntent.send();
+                } catch (PendingIntent.CanceledException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
--- app/src/main/java/org/onpanic/hiddenbackup/receivers/RunBackupReceiver.java
+++ app/src/main/java/org/onpanic/hiddenbackup/receivers/RunBackupReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 
@@ -28,29 +30,42 @@ public class RunBackupReceiver extends BroadcastReceiver {
         preferences = PreferenceManager.getDefaultSharedPreferences(context);
         localBroadcastManager = LocalBroadcastManager.getInstance(context);
 
-        backupFinish = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                boolean forgot = intent.getAction().equals(HiddenBackupConstants.PANIC_BACKUP)
-                        && preferences.getBoolean(context.getString(R.string.pref_forgot_server), false);
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
 
-                if (forgot) {
-                    ServerSettingsHelper.forgot(context);
+        boolean delayed = (activeNetInfo == null)
+                || !activeNetInfo.isConnected()
+                || (preferences.getBoolean(context.getString(R.string.pref_wait_for_wifi), true) && activeNetInfo.getType() != ConnectivityManager.TYPE_WIFI);
+
+        if (delayed) {
+            SharedPreferences.Editor edit = preferences.edit();
+            edit.putBoolean(context.getString(R.string.pref_scheduler_delayed), true);
+            edit.apply();
+        } else {
+            backupFinish = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    boolean forgot = intent.getAction().equals(HiddenBackupConstants.PANIC_BACKUP)
+                            && preferences.getBoolean(context.getString(R.string.pref_forgot_server), false);
+
+                    if (forgot) {
+                        ServerSettingsHelper.forgot(context);
+                    }
+
+                    localBroadcastManager.unregisterReceiver(backupFinish);
+                    if (preferences.getBoolean(context.getString(R.string.pref_runned_notification), false)) {
+                        TriggerNotification notification = new TriggerNotification(context);
+                        notification.show();
+                    }
                 }
+            };
 
-                localBroadcastManager.unregisterReceiver(backupFinish);
-                if (preferences.getBoolean(context.getString(R.string.pref_runned_notification), false)) {
-                    TriggerNotification notification = new TriggerNotification(context);
-                    notification.show();
-                }
-            }
-        };
+            localBroadcastManager.registerReceiver(
+                    backupFinish, new IntentFilter(HiddenBackupConstants.BACKUP_FINISH));
 
-        localBroadcastManager.registerReceiver(
-                backupFinish, new IntentFilter(HiddenBackupConstants.BACKUP_FINISH));
-
-        Intent backup = new Intent(context, OrbotService.class);
-        backup.setAction(HiddenBackupConstants.FULL_BACKUP);
-        context.startService(backup);
+            Intent backup = new Intent(context, OrbotService.class);
+            backup.setAction(HiddenBackupConstants.FULL_BACKUP);
+            context.startService(backup);
+        }
     }
 }
